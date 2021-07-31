1ff52a8df04c3b86b3b23081d5af9e6abcdf2bec
U-LAPTOP-NJSVVDN9\LENOVO <akshaykaware20@gmail.com>
2021-07-31 21:02:59 +0530

    Third Upload

--- src/main/java/com/pack/revature/Main.java
+++ src/main/java/com/pack/revature/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Result is : " +checkIfInputIsAnEvenNumber(122)); 
         System.out.println("Hello World");// Testing in the main method
+        System.out.println("Hello Bharat mata");
     }
     public static boolean checkIfInputIsAnEvenNumber(int number){
         return number % 2 == 0;
